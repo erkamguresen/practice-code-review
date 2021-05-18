d805c0911a0a2159680607cf438ab0ae58ddeb74
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-18 10:40:49 +0200

    code linted

--- src/difference/difference.js
+++ src/difference/difference.js
@@ -1,17 +1,17 @@
 /**
- * This function creates an array of values that are in the first array, 
+ * This function creates an array of values that are in the first array,
  * but not not in the second array.
  *
- * Repeated values are not duplicated in the return value, 
+ * Repeated values are not duplicated in the return value,
  * and the order of result values are determined by the first array.
  *
  * **Note:** This function returns a new array, and has no side-effects.
  *
  * @param {Array} [array=[]] - The array to inspect.
  * @param {Array} [values=[]] - The values to exclude.
- * 
+ *
  * @returns {Array} Returns the new array of filtered values.
- * 
+ *
  * @example
  *
  * difference([2, 1], [2, 3]); //  [1]
@@ -21,11 +21,12 @@
  * difference([1, 2, 1], [2, 3]); //  [1]
  */
 export const arrayDifference = (array = [], values = []) => {
-    let returnArray = [];
-    for (let i = 0; i < array.length; i++) {
-      const element = array[i];
-      if (!values.includes(element) && !returnArray.includes(element))
-        returnArray.push(element);
+  const returnArray = [];
+  for (let i = 0; i < array.length; i++) {
+    const element = array[i];
+    if (!values.includes(element) && !returnArray.includes(element)) {
+      returnArray.push(element);
     }
-    return returnArray;
-  };
+  }
+  return returnArray;
+};
