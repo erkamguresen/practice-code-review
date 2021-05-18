ca6a7cfb33fa861c0121f2aa94a41d2d5cbe4660
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-18 09:50:21 +0200

    code

--- src/difference/difference.js
+++ src/difference/difference.js
@@ -1,4 +1,12 @@
 /**
  *
  */
-export const solutionName = () => {};
+ export const arrayDifference = (array = [], values = []) => {
+    let returnArray = [];
+    for (let i = 0; i < array.length; i++) {
+      const element = array[i];
+      if (!values.includes(element) && !returnArray.includes(element))
+        returnArray.push(element);
+    }
+    return returnArray;
+  };
