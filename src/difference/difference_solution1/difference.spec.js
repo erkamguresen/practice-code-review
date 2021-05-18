a2497871c44c30be57ca735e27ed4da19313d176
ERKAM GURESEN <erkamguresen@gmail.com>
2021-05-18 07:42:45 -0700

    test

--- src/difference/difference_solution1/difference.spec.js
+++ src/difference/difference_solution1/difference.spec.js
@@ -19,4 +19,42 @@ describe('extreme cases', () => {
       expect(actual).toEqual([]);
     });
   });
+  describe('repeating elements', () => {
+    it('should not contain repeated elements from the array', () => {
+      const actual = arrayDifference([1, 2, 1], [2, 3]);
+      expect(actual).toEqual([1]);
+    });
+    it('should not contain repeated elements from the values', () => {
+      const actual = arrayDifference([1, 2, 1], [2, 2, 3]);
+      expect(actual).toEqual([1]);
+    });
+  });
+});
+
+describe('normal cases', () => {
+  it('should return [1]', () => {
+    const actual = arrayDifference([2, 1], [2, 3]);
+    expect(actual).toEqual([1]);
+  });
+  it('should return [2]', () => {
+    const actual = arrayDifference([2, 1], [1, 3, 4]);
+    expect(actual).toEqual([2]);
+  });
+  it('should include [1] and [4]', () => {
+    const actual = arrayDifference([3, 2, 4, 1], [2, 3]);
+    expect(actual.includes(1)).toEqual(true);
+    expect(actual.includes(4)).toEqual(true);
+  });
+  describe('side effects', () => {
+    it('should not have side effects on the array', () => {
+      const actual = [1, 2, 3];
+      arrayDifference(actual, [2, 3]);
+      expect(actual).toEqual([1, 2, 3]);
+    });
+    it('should not have side effects on the values', () => {
+      const actual = [2, 3];
+      arrayDifference([1, 2, 3], actual);
+      expect(actual).toEqual([2, 3]);
+    });
+  });
 });
