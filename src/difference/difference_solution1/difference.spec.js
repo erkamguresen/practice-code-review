92b2391b529f2d41571c5a7732ad4394aa8421b2
ERKAM GURESEN <erkamguresen@gmail.com>
2021-05-18 07:41:36 -0700

    test cases

--- src/difference/difference_solution1/difference.spec.js
+++ src/difference/difference_solution1/difference.spec.js
@@ -2,12 +2,20 @@ import { arrayDifference } from './difference.js';
 
 describe('extreme cases', () => {
   describe('empty inputs', () => {
-    it('should return empty for an empty array', () => {
+    it('should return empty for an empty array and default values', () => {
       const actual = arrayDifference([]);
       expect(actual).toEqual([]);
     });
     it('should return empty for an empty array and an empty values', () => {
       const actual = arrayDifference([], []);
+      expect(actual).toEqual([]);
+    });
+    it('should return empty for an empty array', () => {
+      const actual = arrayDifference([], [1, 2, 1]);
+      expect(actual).toEqual([]);
+    });
+    it('should return empty for default values', () => {
+      const actual = arrayDifference();
       expect(actual).toEqual([]);
     });
   });
--- src/difference/difference_solution1/sandbox.js
+++ src/difference/difference_solution1/sandbox.js
@@ -1,4 +1,4 @@
-import { difference } from './difference';
+import { arrayDifference as difference } from './difference.js';
 
 /*
   anything goes in this file, it's yours to make mistakes and be messy
@@ -10,17 +10,6 @@ import { difference } from './difference';
     the main file should include only the exported function and jsdoc string
 */
 
-console.log(sortNumbers([1, 2, 3])); // [1,2,3]
+console.log(difference([1, 2, 1], [2, 3])); //  [1]
 
-const anArray = [1, 0, -1];
-console.log(sortNumbers(anArray) !== anArray); // true, returns a new array
-console.log(anArray); // [1, 0 , -1]
-
-// --- strange, the default sort is alphabetical ---
-
-const defaultSort = (arr) => {
-  return [...arr].sort();
-};
-
-// would have expected this to log [-3, -2, -1]
-console.log(defaultSort([-1, -2, -3])); // [-1, -2, -3], what?
+console.log(difference([2, 1], [1, 3, 4])); //  [1]
