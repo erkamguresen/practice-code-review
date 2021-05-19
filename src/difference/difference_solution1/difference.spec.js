0d0dceb75e4b771f506be9f44d161a7a15aa92e6
ERKAM GURESEN <46300684+erkamguresen@users.noreply.github.com>
2021-05-19 10:27:51 +0200

    comments are added

--- src/difference/difference_solution1/difference.spec.js
+++ src/difference/difference_solution1/difference.spec.js
@@ -40,12 +40,19 @@ describe('normal cases', () => {
     const actual = arrayDifference([2, 1], [1, 3, 4]);
     expect(actual).toEqual([2]);
   });
-  it('should include [1] and [4]', () => {
+  it('should include [1]', () => {
     const actual = arrayDifference([3, 2, 4, 1], [2, 3]);
     expect(actual.includes(1)).toEqual(true);
+  });
+  it('should include [4]', () => {
+    const actual = arrayDifference([3, 2, 4, 1], [2, 3]);
     expect(actual.includes(4)).toEqual(true);
   });
-  describe('side effects', () => {
+  it('should not include [4]', () => {
+    const actual = arrayDifference([3, 2, 4, 1], [2, 3, 4, 4]);
+    expect(!actual.includes(4)).toEqual(true);
+  });
+  describe('have no side effects', () => {
     it('should not have side effects on the array', () => {
       const actual = [1, 2, 3];
       arrayDifference(actual, [2, 3]);
